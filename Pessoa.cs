eecf2067dbbd0600fd05d50531e935c81bab6dfa
gothwavy <ryanalmeidadev@gmail.com>
2024-12-22 17:56:00 -0300

    Finalizado✅

---
+++ Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ExercicioIMC
+{
+    internal class Pessoa
+    {
+        public double peso, altura;
+
+        public double RetornarIMC()
+        {
+            return peso / (altura * altura); ;
+        }
+
+        public string Resultado(double imc)
+        {
+            string mensagem;
+
+            if (imc < 18.5)
+            {
+                mensagem = "Abaixo do peso!";
+            }
+            else if (imc < 25)
+            {
+                mensagem = "Peso normal!";
+            }
+            else if (imc < 30)
+            {
+                mensagem = "Acima do peso!";
+            }
+            else if (imc < 35)
+            {
+                mensagem = "Obesidade I!";
+            }
+            else if (imc < 40)
+            {
+                mensagem = "Obesidade II!";
+            }
+            else
+            {
+                mensagem = "Obesidade III!";
+            }
+
+            return mensagem;
+        }
+
+        public void Mensagem()
+        {
+            string resultadoFinal = Resultado(RetornarIMC());
+            double calculoIMC = RetornarIMC();
+
+            Console.WriteLine($"O seu imc e igual a: {calculoIMC}\nSeu resultado e: {resultadoFinal}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _04_ExercicioIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa ryan = new Pessoa();
+            ryan.peso = 80;
+            ryan.altura = 1.8;
+            ryan.Mensagem();
+        }
+    }
+}
